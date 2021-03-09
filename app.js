7d8968d030b7e53433bc99cd8a92faba22ab9151
Emils Strelis <emilstrelis@inbox.lv>
2021-03-09 15:38:57 +0000

    Izmainas 3

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ const search = document.getElementById('search-books');
 const bookList = document.getElementById('book-list');
 
 console.log(search, bookList);
+const titles = document.getElementsByClassName('title');
+
+console.log(Array.isArray(titles));
+console.log(Array.isArray(Array.from(titles)));
+
+Array.from(titles).forEach(function(title){
+  console.log(title);
+});
