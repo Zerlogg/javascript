245cd524d8768d58bc52f780e99c27f9aea3fe1e
Emils Strelis <emilstrelis@inbox.lv>
2021-03-09 16:22:44 +0000

    Izmainas 9

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 const bookList = document.querySelector('#book-list');
+const listItems = document.querySelectorAll('#book-list ul li');
 
-console.log('book list parent element:', bookList.parentElement);
-console.log('book list parent node:', bookList.parentNode);
 console.log('#book-list next sibling:', bookList.nextSibling);
 console.log('#book-list next element sibling:', bookList.nextElementSibling);
 console.log('#book-list previous sibling:', bookList.previousSibling);
 console.log('#book-list previous element sibling:', bookList.previousElementSibling);
+Array.from(listItems).forEach(function(item){
+  item.addEventListener('click', (e) => {
 
-console.log('all node children:');
-Array.from(bookList.childNodes).forEach(function(node){
-  console.log(node);
-});
-
-console.log('all element children:');
-Array.from(bookList.children).forEach(function(node){
-  console.log(node);
-});
-
-const titles = bookList.querySelectorAll('.name');
-
-console.log('Book titles:');
-Array.from(titles).forEach(function(title){
-  console.log(title.textContent);
-}); 
 bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool for everyone else!'; 
+    const li = e.target.parentElement;
+    console.log('child element to remove:', li);
+    console.log('parent element to remove child from:', li.parentElement);
+    li.parentNode.removeChild(li);
+
+  });
+});
+
+// prevent default behaviour
+
+const link = document.querySelector('#page-banner a');
+
+link.addEventListener('click', function(e){
+  e.preventDefault();
+  console.log('Navigation to', e.target.textContent, 'was prevented');
+}); 
