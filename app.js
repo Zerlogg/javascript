0e27a43b822d08e8939fdb07d1a24578c4529da9
Emils Strelis <emilstrelis@inbox.lv>
2021-03-09 16:00:53 +0000

    Izmainas 6

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-const wmf = document.querySelector('#book-list li:nth-child(2) .name');
-console.log(wmf);
-
-var books = document.querySelector('#book-list li .name');
-console.log(books);
-
-books = document.querySelectorAll('#book-list li .name');
-console.log(books);
 const books = document.querySelectorAll('#book-list li .name');
+const banner = document.querySelector('#page-banner');
 
 Array.from(books).forEach(function(book){
-  console.log(book);
-}); 
   book.textContent += ' (Book title)';
 });
+console.log('#page-banner node type is:', banner.nodeType);
+console.log('#page-banner node name is:', banner.nodeName);
+console.log('#page-banner has child nodes:', banner.hasChildNodes());
 
 const bookList = document.querySelector('#book-list');
 bookList.innerHTML = '<h2>Books and more books...</h2>';
-bookList.innerHTML += '<p>This is how you add HTML content</p>';
+bookList.innerHTML += '<p>This is how you add HTML content</p>'; 
+const clonedBanner = banner.cloneNode(true);
+console.log(clonedBanner); 
