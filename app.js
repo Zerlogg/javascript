dd894e6015acc428d0f5f01e8959888f2951a943
Emils Strelis <emilstrelis@inbox.lv>
2021-03-09 16:28:56 +0000

    Izmainas 10

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-const bookList = document.querySelector('#book-list');
 const listItems = document.querySelectorAll('#book-list ul li');
 
-console.log('#book-list next sibling:', bookList.nextSibling);
-console.log('#book-list next element sibling:', bookList.nextElementSibling);
-console.log('#book-list previous sibling:', bookList.previousSibling);
-console.log('#book-list previous element sibling:', bookList.previousElementSibling);
 Array.from(listItems).forEach(function(item){
   item.addEventListener('click', (e) => {
+const list = document.querySelector('#book-list ul');
 
-bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool for everyone else!'; 
+// delete books
+list.addEventListener('click', (e) => {
+  if(e.target.className == 'delete'){
     const li = e.target.parentElement;
     console.log('child element to remove:', li);
     console.log('parent element to remove child from:', li.parentElement);
@@ -24,4 +22,5 @@ const link = document.querySelector('#page-banner a');
 link.addEventListener('click', function(e){
   e.preventDefault();
   console.log('Navigation to', e.target.textContent, 'was prevented');
+  }
 }); 
