95d2c0a81dece0f6613a6cfa34dc3a7b2195417c
Emils Strelis <emilstrelis@inbox.lv>
2021-03-09 16:07:32 +0000

    Izmainas 7

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-const books = document.querySelectorAll('#book-list li .name');
 const banner = document.querySelector('#page-banner');
+const bookList = document.querySelector('#book-list');
 
-Array.from(books).forEach(function(book){
-  book.textContent += ' (Book title)';
-});
 console.log('#page-banner node type is:', banner.nodeType);
 console.log('#page-banner node name is:', banner.nodeName);
 console.log('#page-banner has child nodes:', banner.hasChildNodes());
+console.log('book list parent element:', bookList.parentElement);
+console.log('book list parent node:', bookList.parentNode);
 
-const bookList = document.querySelector('#book-list');
-bookList.innerHTML = '<h2>Books and more books...</h2>';
-bookList.innerHTML += '<p>This is how you add HTML content</p>'; 
 const clonedBanner = banner.cloneNode(true);
 console.log(clonedBanner); 
+console.log('all node children:');
+Array.from(bookList.childNodes).forEach(function(node){
+  console.log(node);
+});
+
+console.log('all element children:');
+Array.from(bookList.children).forEach(function(node){
+  console.log(node);
+});
+
+const titles = bookList.querySelectorAll('.name');
+
+console.log('Book titles:');
+Array.from(titles).forEach(function(title){
+  console.log(title.textContent);
+}); 
